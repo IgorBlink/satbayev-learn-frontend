81af8a9293bd2310958908d0065db8ac5a698c05
IgorBlink <igorblink2@gmail.com>
2024-11-08 09:43:15 +0500

    123
    123

--- src/api/api.js
+++ src/api/api.js
@@ -1,10 +1,10 @@
 export const BASE_URL = "https://dl-back.km-dev.tech"
 export const SKILLS_URL = "https://cd23-2-133-130-122.ngrok-free.app"
-const token = window.btoa(window.Telegram.WebApp.initData)
+const token = 'cXVlcnlfaWQ9QUFGRUR6Uk5BQUFBQUVRUE5FMG50bzRIJnVzZXI9JTdCJTIyaWQlMjIlM0ExMjk1MjU3NDEyJTJDJTIyZmlyc3RfbmFtZSUyMiUzQSUyMkRhbmlpbCUyMiUyQyUyMmxhc3RfbmFtZSUyMiUzQSUyMiUyMiUyQyUyMnVzZXJuYW1lJTIyJTNBJTIyZDlBMm5ZM0ElMjIlMkMlMjJsYW5ndWFnZV9jb2RlJTIyJTNBJTIyZW4lMjIlMkMlMjJpc19wcmVtaXVtJTIyJTNBdHJ1ZSUyQyUyMmFsbG93c193cml0ZV90b19wbSUyMiUzQXRydWUlN0QmYXV0aF9kYXRlPTE3MjkwMDQxODgmaGFzaD1iYWQ4MGY2ZWRjMjQyOGFmYTg1YjUwOTlhNDAyMGNkMjI0YjRlOWQ4N2FiOTYzMmVmNGYxMGExNGRmNzRiNDNj'
 
 // Base fetch function for main API
 export const fetchData = async (url, method, body = {}) => {
-    if(method === "GET") {
+    if(method === "GET") {f
         const response = await fetch(BASE_URL+url, {
             headers: {
                 "ngrok-skip-browser-warning": "1",
